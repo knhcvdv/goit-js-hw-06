b94a60f6f63c2fa30eb9edb143b768678d81df6f
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-06-21 00:27:19 +0300

    task 1,2,3,4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const counterValueElem = document.querySelector('#value');
+const btnDecrementElem = document.querySelector('button[data-action="decrement"]');
+const btnIncrementElem = document.querySelector('button[data-action="increment"]');
+let counterValue = 0;
+
+
+const onClickIncrement = () => {
+        counterValue += 1;
+    counterValueElem.textContent = counterValue;
+};
+
+const onClickDecrement = () => {
+        counterValue -= 1;
+        counterValueElem.textContent = counterValue
+};
+
+
+btnDecrementElem.addEventListener("click", onClickDecrement);
+btnIncrementElem.addEventListener('click', onClickIncrement);
