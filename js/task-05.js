a091633d6f12300425d269752adc224c34e7bb33
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-06-21 19:35:29 +0300

    task 1,2,3,4,5,6

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const inputElem = document.querySelector('#name-input');
+const outputElem = document.querySelector('#name-output');
+
+const onOutputElem = event => {
+    if (event.currentTarget.value !== '') {
+        outputElem.textContent = event.currentTarget.value;
+        } else {
+            outputElem.textContent = 'Anonymous';
+        }
+    };
+inputElem.addEventListener('input', onOutputElem);
+ 
