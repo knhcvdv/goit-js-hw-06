1008c02f345204d5813fdfad3bcf06f39b99fb93
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-06-22 00:26:32 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginFormElem = document.querySelector('.login-form');
+const emailInputElem = document.querySelector('input[name="email"]');
+const passwordInputElem = document.querySelector('input[name="password"]');
 
+const handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (emailInputElem.value === '' || passwordInputElem.value === '') {
+        alert('Please fill in all fields');
+    } else {
+        const user = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(user);
+        event.currentTarget.reset();
+    }
+}
+
+loginFormElem.addEventListener('submit', handleSubmit);
