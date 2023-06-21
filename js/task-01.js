8223e5aee9fbaee52ad564de7fcd0b13d200efb6
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-06-21 22:26:39 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -5,4 +5,4 @@ const itemsElem = document.querySelectorAll(".item");
 itemsElem.forEach((item)=>{
     console.log("Category:",item.firstElementChild.textContent);
     console.log("Elements:",item.lastElementChild.children.length);
-})
+}) 
