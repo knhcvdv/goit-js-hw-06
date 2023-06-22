555906e96fbb5c82ad939ff1e991542b4d387c2c
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-06-22 16:25:59 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const changeColorButtonElem = document.querySelector('.change-color');
+const colorNameElem = document.querySelector('.color');
+const bodyElem = document.querySelector('body');
+
+const handleColorChange = () => {
+  colorNameElem.textContent = getRandomHexColor();
+  bodyElem.style.backgroundColor = getRandomHexColor();
+}
+
+changeColorButtonElem.addEventListener('click', handleColorChange);
